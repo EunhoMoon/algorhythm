7c502431c1108151f52c3b6ab66463c308b9abb7
EunhoMoon <eunho.moon@gmail.com>
2024-08-16 15:07:51 +0900

    20240816 - 나는야 포켓몬 마스터 이다솜

---
+++ baekjoon/1620.py
@@ -0,0 +1,21 @@
+"""
+https://www.acmicpc.net/problem/1620
+ - 시간 복잡도를 줄이기 위해 index 조회는 list, 이름 조회는 dict
+"""
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+pokemon_list = []
+pokemon_dict = {}
+
+for idx in range(1, N + 1):
+    name = input().rstrip()
+    pokemon_list.append(name)
+    pokemon_dict[name] = idx
+
+for _ in range(M):
+    command = input().rstrip()
+    print(pokemon_list[int(command) - 1] if command.isdigit() else pokemon_dict[command])
