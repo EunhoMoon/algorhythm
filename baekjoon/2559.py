931ebd5ad0715a78da307ca3f2392a3622c27290
EunhoMoon <eunho.moon@gmail.com>
2024-08-16 11:13:03 +0900

    20240816 - 수열: 슬라이딩 윈도우

--- baekjoon/2559.py
+++ baekjoon/2559.py
@@ -9,13 +9,11 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
 
-old_val = sum(arr[0:K])
-result = old_val
+current_sum = sum(arr[:K])
+max_sum = current_sum
 
-for i in range(1, len(arr) - K + 1):
-    new_val = old_val - arr[i - 1] + arr[i + K - 1]
-    if result < new_val:
-        result = new_val
-    old_val = new_val
+for i in range(K, N):
+    current_sum = current_sum - arr[i - K] + arr[i]
+    max_sum = max(current_sum, max_sum)
 
-print(result)
+print(max_sum)
