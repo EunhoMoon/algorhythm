c5de12ad26c2ef0c8a58847167d202d7339712dd
EunhoMoon <eunho.moon@gmail.com>
2024-10-31 20:44:15 +0900

    20241031 - 수리공 항승: 탐욕법

---
+++ baekjoon/1449.py
@@ -0,0 +1,18 @@
+
+"""
+https://www.acmicpc.net/problem/1449
+"""
+import sys
+
+read_line = sys.stdin.readline
+N, L = map(int, read_line().split())
+
+start_pipe:int = 0
+result:int = 0
+
+for pipe in sorted(map(int, read_line().split())):
+    if not (start_pipe > 0 and pipe - (L - 1) <= start_pipe) :
+        start_pipe = pipe
+        result += 1
+
+print(result)
