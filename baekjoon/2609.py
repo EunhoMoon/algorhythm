c6a4888149c3f3df6d05e7918ca65bf9e79a4cdb
Janek <eunho.moon@gmail.com>
2024-11-10 18:38:23 +0900

    20241110 - 문제 풀이 연습

--- baekjoon/2609.py
+++ baekjoon/2609.py
@@ -2,16 +2,15 @@
 https://www.acmicpc.net/problem/2609
 """
 
-import sys
-
 num1, num2 = map(int, input().split())
 
-min_num = max(num1, num2)
-max_num = min(num1, num2)
-
-for i in range(min(num1, num2), 1, -1):
-    print(i)
+gcd = 1
+for i in range(min(num1, num2), 0, -1):
     if num1 % i == 0 and num2 % i == 0:
-        min_num = min(i, min_num)
+        gcd = i
+        break
 
-print(min_num)
+lcm = (num1 * num2) // gcd
+
+print(gcd)
+print(lcm)
