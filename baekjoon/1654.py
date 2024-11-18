99fde674d6288d574d339c8571deb241983370c5
EunhoMoon <eunho.moon@gmail.com>
2024-11-18 17:18:46 +0900

    20241118 - 문제 풀이 연습

---
+++ baekjoon/1654.py
@@ -0,0 +1,27 @@
+"""
+https://www.acmicpc.net/problem/1654
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+K, N = map(int, input().split())
+
+lines = [int(input()) for _ in range(K)]
+start = 1
+end = max(lines)
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = sum(line // mid for line in lines)
+
+    if count >= N:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+
+print(result)
