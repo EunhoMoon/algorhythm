914cef85554ae75258d0c90e8795877bece99089
EunhoMoon <eunho.moon@gmail.com>
2024-11-29 10:56:12 +0900

    20241129 - 문제 풀이 연습: 그리디

--- baekjoon/2217.py
+++ baekjoon/2217.py
@@ -7,15 +7,11 @@ from collections import deque
 
 input = sys.stdin.readline
 
-N = int(input())
-ropes = deque(sorted([int(input()) for _ in range(N)]))
+ropes = deque(sorted([int(input()) for _ in range(int(input()))]))
 max_weight = 0
 
 while ropes:
-    ropes_len = len(ropes)
-    new_rope = ropes.popleft()
-
-    new_max_weight = new_rope * ropes_len
+    new_max_weight = ropes.popleft() * (len(ropes) + 1)
     max_weight = new_max_weight if new_max_weight > max_weight else max_weight
 
 print(max_weight)
