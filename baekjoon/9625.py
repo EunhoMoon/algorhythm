6c5283bc42328cf94ef2e1a55012cde3a456dae9
EunhoMoon <eunho.moon@gmail.com>
2024-12-04 15:19:05 +0900

    20241204 - 문제 풀이 연습: dp

--- baekjoon/9625.py
+++ baekjoon/9625.py
@@ -4,9 +4,9 @@ https://www.acmicpc.net/problem/9625
 
 K = int(input())
 
-count_A, count_B = 0, 1  # 초기 값 (K=1)
+count_A, count_B = 0, 1
 
-for _ in range(2, K + 1):  # K 단계까지 반복
+for _ in range(2, K + 1):
     count_A, count_B = count_B, count_A + count_B
 
 print(f"{count_A} {count_B}")
